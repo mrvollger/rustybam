6f5e2f233f190c27482fd019ccf754e1d531902e
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-07-13 16:49:42 -0700

    starting to add overlaps for bed annotations

--- src/annotate.rs
+++ src/annotate.rs
@@ -3,11 +3,11 @@ use bio::io::*;
 use itertools::Itertools;
 
 pub trait IntervalTreeExt<N: Ord + Clone, D> {
-    fn find_bed_overlaps(&self, rec: bed::Record) -> Vec<Entry<u64, bed::Record>>;
+    fn find_bed_overlaps(&self, rec: &bed::Record) -> Vec<Entry<u64, bed::Record>>;
 }
 
-impl IntervalTreeExt<u64, bed::Record> for IntervalTree<u64, bed::Record> {
-    fn find_bed_overlaps(&self, rec: bed::Record) -> Vec<Entry<u64, bed::Record>> {
+impl IntervalTreeExt<u64, &bed::Record> for IntervalTree<u64, bed::Record> {
+    fn find_bed_overlaps(&self, rec: &bed::Record) -> Vec<Entry<u64, bed::Record>> {
         self.find(rec.start()..rec.end())
             .filter(|entry| entry.data().chrom() == rec.chrom())
             .collect_vec()
@@ -45,17 +45,17 @@ mod tests {
         tree.insert(11..20, rec.clone());
         tree.insert(10..30, rec2.clone());
 
-        for r in tree.find_bed_overlaps(rec) {
-            assert_eq!(r.interval(), &(Interval::from(11..20)));
+        for r in tree.find_bed_overlaps(&rec) {
+            assert_eq!(r.interval(), &(Interval::from(rec.start()..rec.end())));
             assert_eq!(r.data().chrom(), "Range_1");
             eprintln!("found filtered range");
         }
         for r in tree.find(15..25) {
             if r.data().chrom() == "Range_1" {
-                assert_eq!(r.interval(), &(Interval::from(11..20)));
+                assert_eq!(r.interval(), &(Interval::from(rec.start()..rec.end())));
                 assert_eq!(r.data().chrom(), "Range_1");
             } else if r.data().chrom() == "Range_2" {
-                assert_eq!(r.interval(), &(Interval::from(10..30)));
+                assert_eq!(r.interval(), &(Interval::from(rec2.start()..rec2.end())));
                 assert_eq!(r.data().chrom(), "Range_2");
             }
         }
