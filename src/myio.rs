73afaed2a707c37b2a17160236428e2f3c014b9d
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-09-17 08:50:21 -0700

    adding in pigz

--- src/myio.rs
+++ src/myio.rs
@@ -1,11 +1,19 @@
 use flate2::read;
+/*use flate2::read::MultiGzDecoder;
 use flate2::write;
+use flate2::write::DeflateDecoder;*/
 use flate2::Compression;
+use gzp::deflate::Bgzf; //, Gzip, Mgzip, RawDeflate};
+use gzp::ZBuilder;
 use std::ffi::OsStr;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
-/// code from https://users.rust-lang.org/t/write-to-normal-or-gzip-file-transparently/35561/2
+
+const BUFFERSIZE: usize = 64 * 1024;
+
+/// code modified from https://users.rust-lang.org/t/write-to-normal-or-gzip-file-transparently/35561/2
+/// to work with gzp
 
 /// Write normal or compressed files seamlessly
 /// Uses the presence of a `.gz` extension to decide
@@ -16,13 +24,16 @@ pub fn writer(filename: &str) -> Box<dyn Write> {
         Err(why) => panic!("couldn't open {}: {}", path.display(), why.to_string()),
         Ok(file) => file,
     };
-
     if path.extension() == Some(OsStr::new("gz")) {
         // Error is here: Created file isn't gzip-compressed
-        Box::new(BufWriter::with_capacity(
-            128 * 1024,
-            write::GzEncoder::new(file, Compression::default()),
-        ))
+        let buffer = Box::new(BufWriter::with_capacity(BUFFERSIZE, file));
+        let writer = ZBuilder::<Bgzf, _>::new()
+            .num_threads(8)
+            .compression_level(Compression::new(9))
+            .from_writer(buffer);
+        Box::new(writer)
+        //let writer = write::GzEncoder::new(file, Compression::default());
+        //Box::new(BufWriter::with_capacity(128 * 1024, writer))
     } else {
         Box::new(BufWriter::with_capacity(128 * 1024, file))
     }
