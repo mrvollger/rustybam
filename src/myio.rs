f27357df37c1de6f486ba90c71b9c547bf051b03
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-09-17 10:42:48 -0700

    trying to make unizerval gzip reader and writer

--- src/myio.rs
+++ src/myio.rs
@@ -1,59 +1,82 @@
+use anyhow::Result;
 use flate2::read;
-/*use flate2::read::MultiGzDecoder;
-use flate2::write;
-use flate2::write::DeflateDecoder;*/
 use flate2::Compression;
 use gzp::deflate::Bgzf; //, Gzip, Mgzip, RawDeflate};
 use gzp::ZBuilder;
 use std::ffi::OsStr;
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
+use std::path::PathBuf;
 
-const BUFFERSIZE: usize = 64 * 1024;
-
-/// code modified from https://users.rust-lang.org/t/write-to-normal-or-gzip-file-transparently/35561/2
-/// to work with gzp
+const BUFFER_SIZE: usize = 128 * 1024;
 
 /// Write normal or compressed files seamlessly
 /// Uses the presence of a `.gz` extension to decide
 // Attempting to have a file writer too
 pub fn writer(filename: &str) -> Box<dyn Write> {
-    let path = Path::new(filename);
-    let file = match File::create(&path) {
-        Err(why) => panic!("couldn't open {}: {}", path.display(), why.to_string()),
-        Ok(file) => file,
-    };
-    if path.extension() == Some(OsStr::new("gz")) {
+    let ext = Path::new(filename).extension();
+    let path = PathBuf::from(filename);
+    let buffer = get_output(Some(path)).expect("Error: cannot create output file");
+
+    if ext == Some(OsStr::new("gz")) {
         // Error is here: Created file isn't gzip-compressed
-        let buffer = Box::new(BufWriter::with_capacity(BUFFERSIZE, file));
         let writer = ZBuilder::<Bgzf, _>::new()
             .num_threads(8)
-            .compression_level(Compression::new(9))
+            .compression_level(Compression::new(6))
             .from_writer(buffer);
         Box::new(writer)
-        //let writer = write::GzEncoder::new(file, Compression::default());
-        //Box::new(BufWriter::with_capacity(128 * 1024, writer))
     } else {
-        Box::new(BufWriter::with_capacity(128 * 1024, file))
+        buffer
     }
 }
 
 /// Read normal or compressed files seamlessly
 /// Uses the presence of a `.gz` extension to decide
 pub fn reader(filename: &str) -> Box<dyn BufRead> {
-    let path = Path::new(filename);
-    let file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", path.display(), why.to_string()),
-        Ok(file) => file,
-    };
+    let ext = Path::new(filename).extension();
+    let path = PathBuf::from(filename);
 
-    if path.extension() == Some(OsStr::new("gz")) {
+    if ext == Some(OsStr::new("gz")) {
+        let file = match File::open(&path) {
+            Err(why) => panic!("couldn't open {}: {}", path.display(), why.to_string()),
+            Ok(file) => file,
+        };
         Box::new(BufReader::with_capacity(
-            128 * 1024,
+            BUFFER_SIZE,
             read::GzDecoder::new(file),
         ))
     } else {
-        Box::new(BufReader::with_capacity(128 * 1024, file))
+        get_input(Some(path)).expect("Error: cannot read input file")
     }
+}
+
+/// Get a buffered output writer from stdout or a file
+pub fn get_output(path: Option<PathBuf>) -> Result<Box<dyn Write + Send + 'static>> {
+    let writer: Box<dyn Write + Send + 'static> = match path {
+        Some(path) => {
+            if path.as_os_str() == "-" {
+                Box::new(BufWriter::with_capacity(BUFFER_SIZE, io::stdout()))
+            } else {
+                Box::new(BufWriter::with_capacity(BUFFER_SIZE, File::create(path)?))
+            }
+        }
+        None => Box::new(BufWriter::with_capacity(BUFFER_SIZE, io::stdout())),
+    };
+    Ok(writer)
+}
+
+/// Get a bufferd input reader from stdin or a file
+pub fn get_input(path: Option<PathBuf>) -> Result<Box<dyn BufRead + Send + 'static>> {
+    let reader: Box<dyn BufRead + Send + 'static> = match path {
+        Some(path) => {
+            if path.as_os_str() == "-" {
+                Box::new(BufReader::with_capacity(BUFFER_SIZE, io::stdin()))
+            } else {
+                Box::new(BufReader::with_capacity(BUFFER_SIZE, File::open(path)?))
+            }
+        }
+        None => Box::new(BufReader::with_capacity(BUFFER_SIZE, io::stdin())),
+    };
+    Ok(reader)
 }
