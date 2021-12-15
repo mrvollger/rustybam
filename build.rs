2a589ae6ec882a38d7dc86afb382f360fdd30d33
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-12-14 23:12:15 -0800

    preping for a conda release

--- build.rs
+++ build.rs
@@ -5,7 +5,8 @@ include!("src/cli.rs");
 fn main() {
     let mut app = Cli::into_app();
     let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("target/");
-
+    /*
+    // creates an error with cargo publish and then installing
     for bin_name in ["rb", "rustybam"] {
         // Bash, Fish, Zsh, PowerShell, Elvish
         generate_to(Bash, &mut app, bin_name, &outdir).unwrap();
@@ -14,4 +15,5 @@ fn main() {
         generate_to(PowerShell, &mut app, bin_name, &outdir).unwrap();
         generate_to(Elvish, &mut app, bin_name, &outdir).unwrap();
     }
+    */
 }
