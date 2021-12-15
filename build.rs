63a8ab437f4f04c95aa5c0f6683f3171d47aefd6
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-12-14 23:13:29 -0800

    preping for a conda release

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 // in build.rs
-use clap_generate::{generate_to, generators::*};
+//use clap_generate::{generate_to, generators::*};
 include!("src/cli.rs");
 
 fn main() {
-    let mut app = Cli::into_app();
-    let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("target/");
     /*
     // creates an error with cargo publish and then installing
+    let mut app = Cli::into_app();
+    let outdir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("target/");
     for bin_name in ["rb", "rustybam"] {
         // Bash, Fish, Zsh, PowerShell, Elvish
         generate_to(Bash, &mut app, bin_name, &outdir).unwrap();
