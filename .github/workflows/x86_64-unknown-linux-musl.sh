7e15233050d1aa6906b54f376de389e065a321bb
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-12-16 17:37:17 -0800

    added paf filtering and scaffolding

--- .github/workflows/x86_64-unknown-linux-musl.sh
+++ .github/workflows/x86_64-unknown-linux-musl.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-sudo apt-get install --yes libgsl0-dev
-sudo apt-get install --yes libgsl-dev
+#apt-get install --yes libgsl0-dev
+#apt-get install --yes libgsl-dev
+#apk add --no-cache pkg-config
+apk add --upgrade --no-cache pkg-config gsl-dev
 
-#wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+#wget -qO- https:/micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 #./bin/micromamba create -n rbt bcftools starcode -c conda-forge -c bioconda
 #yes | ./bin/micromamba shell init -s bash -p /home/gitpod/micromamba
